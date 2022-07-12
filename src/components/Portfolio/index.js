3f2611575b51340e4b6e20375d859ab224fafe2b
Mitchell George <mitchgeorge8@gmail.com>
2022-07-11 22:14:14 -0400

    Updating Portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Portfolio() {
   return (
     <>
       <h2 className="text-xl md:text-3xl">Portfolio</h2>
+
+      <div>
+        <div></div>
+      </div>
     </>
   );
 }
